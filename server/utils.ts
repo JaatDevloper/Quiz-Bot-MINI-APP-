99115f580b547c8fa7f61fe55a67588a67f1ea33
.reven <pinterestjat2022@gmail.com>
2025-11-03 23:23:13 +0530

    Update utils.ts

--- server/utils.ts
+++ server/utils.ts
@@ -1,6 +1,9 @@
-// server/utils.ts (NEW FILE)
-// Contains functions that NEVER import 'vite'
+// server/utils.ts
 
+/**
+ * Provides a basic logging function, isolated from development tools like Vite.
+ * This function is safe to be included in the production build.
+ */
 export function log(message: string, source = "express") {
   const formattedTime = new Date().toLocaleTimeString("en-US", {
     hour: "numeric",
